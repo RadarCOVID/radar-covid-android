d44122519545f25363a522b0456cd6136c195dcf
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-06-17 10:42:16 +0200

    *[Poll] Implemented post answers api interface

--- app/src/main/java/com/indra/coronaradar/datamanager/api/ApiInterface.kt
+++ app/src/main/java/com/indra/coronaradar/datamanager/api/ApiInterface.kt
@@ -4,9 +4,18 @@ import com.indra.coronaradar.models.response.ResponseQuestions
 import com.indra.coronaradar.models.response.ResponseSettings
 import com.indra.coronaradar.models.response.ResponseUuid
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ApiInterface {
+
+    companion object {
+
+        const val SEDIA_USER_TOKEN = "SEDIA-UserToken"
+
+    }
 
     @GET("/")
     fun test(): Call<String>
@@ -19,5 +28,11 @@ interface ApiInterface {
 
     @GET("/questionnaire/questions")
     fun getQuestions(): Call<ResponseQuestions>
+
+    @POST("/questionnaire/answers")
+    fun postAnswers(
+        @Header(SEDIA_USER_TOKEN) uuid: String,
+        @Body body: HashMap<String, String>
+    ): Call<ResponseQuestions>
 
 }
