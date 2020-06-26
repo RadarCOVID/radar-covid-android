d40e567fed2cb57264a1c22c7f6969c02f7ee961
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-06-26 12:19:02 +0200

    *[Exposure][0027522] Implemented more info button

--- app/src/main/java/es/gob/radarcovid/features/exposure/router/ExposureRouterImpl.kt
+++ app/src/main/java/es/gob/radarcovid/features/exposure/router/ExposureRouterImpl.kt
@@ -1,6 +1,8 @@
 package es.gob.radarcovid.features.exposure.router
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import es.gob.radarcovid.features.covidreport.form.view.CovidReportActivity
 import es.gob.radarcovid.features.exposure.protocols.ExposureRouter
 import javax.inject.Inject
@@ -8,5 +10,11 @@ import javax.inject.Inject
 class ExposureRouterImpl @Inject constructor(private val context: Context) : ExposureRouter {
 
     override fun navigateToCovidReport() = CovidReportActivity.open(context)
+
+    override fun navigateToBrowser(url: String) {
+        context.startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        })
+    }
 
 }
