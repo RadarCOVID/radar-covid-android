d75858bc4e890e8323649fdab2ec16541dbd74a3
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-06-28 22:13:14 +0200

    *[Confirmation] Implemented more info button

--- app/src/main/java/es/gob/radarcovid/features/covidreport/confirmation/ConfirmationActivity.kt
+++ app/src/main/java/es/gob/radarcovid/features/covidreport/confirmation/ConfirmationActivity.kt
@@ -2,10 +2,12 @@ package es.gob.radarcovid.features.covidreport.confirmation
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import es.gob.radarcovid.R
+import kotlinx.android.synthetic.main.activity_confirmation.*
 
 class ConfirmationActivity : AppCompatActivity() {
 
@@ -20,10 +22,21 @@ class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
+
+        initViews()
     }
 
     fun onBackArrowClick(view: View) {
         onBackPressed()
+    }
+
+    private fun initViews() {
+        buttonMoreInfo.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data =
+                    Uri.parse("https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/ciudadania.htm")
+            })
+        }
     }
 
 }
