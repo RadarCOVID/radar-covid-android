b636d9ab19435eb46689a01c447c4fee34f4847d
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-07-23 08:14:38 +0200

    *[Multilanguage] Implemented languages and regions in a single request

--- app/src/main/java/es/gob/radarcovid/datamanager/usecase/GetLocaleInfoUseCase.kt
+++ app/src/main/java/es/gob/radarcovid/datamanager/usecase/GetLocaleInfoUseCase.kt
@@ -6,7 +6,12 @@ import es.gob.radarcovid.datamanager.mapper.LanguagesDataMapper
 import es.gob.radarcovid.datamanager.mapper.RegionsDataMapper
 import es.gob.radarcovid.datamanager.repository.ContentfulRepository
 import es.gob.radarcovid.models.domain.Language
+import es.gob.radarcovid.models.domain.LocaleInfo
 import es.gob.radarcovid.models.domain.Region
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.functions.BiFunction
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class GetLocaleInfoUseCase @Inject constructor(
@@ -14,6 +19,42 @@ class GetLocaleInfoUseCase @Inject constructor(
     private val languagesDataMapper: LanguagesDataMapper,
     private val regionsDataMapper: RegionsDataMapper
 ) {
+
+    fun observeLocaleInfo(): Observable<LocaleInfo> =
+        Observable.zip<List<Language>, List<Region>, LocaleInfo>(
+            observeLanguages(),
+            observeRegions(),
+            BiFunction { languages, regions ->
+                LocaleInfo(languages, regions)
+            }
+        ).subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+
+    private fun observeLanguages(): Observable<List<Language>> = Observable.create { emitter ->
+        getLanguages(
+            onSuccess = {
+                emitter.onNext(it)
+                emitter.onComplete()
+            },
+            onError = {
+                emitter.onError(it)
+                emitter.onComplete()
+            }
+        )
+    }
+
+    private fun observeRegions(): Observable<List<Region>> = Observable.create { emitter ->
+        getRegions(
+            onSuccess = {
+                emitter.onNext(it)
+                emitter.onComplete()
+            },
+            onError = {
+                emitter.onError(it)
+                emitter.onComplete()
+            }
+        )
+    }
 
     fun getLanguages(onSuccess: (List<Language>) -> Unit, onError: (Throwable) -> Unit) {
         asyncRequest(onSuccess, onError) {
---
+++ app/src/main/java/es/gob/radarcovid/models/domain/LocaleInfo.kt
@@ -0,0 +1,3 @@
+package es.gob.radarcovid.models.domain
+
+data class LocaleInfo(val languages: List<Language>, val regions: List<Region>)
