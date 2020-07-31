468de033cc80a03ae045aa2e493743e22fa996c4
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-07-31 08:57:20 +0200

    *[LegalInfo] Implemented url loading from contentful

--- app/src/main/java/es/gob/radarcovid/features/onboarding/pages/legal/router/LegalInfoRouterImpl.kt
+++ app/src/main/java/es/gob/radarcovid/features/onboarding/pages/legal/router/LegalInfoRouterImpl.kt
@@ -4,17 +4,21 @@ import android.content.Intent
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import es.gob.radarcovid.common.base.Constants
+import es.gob.radarcovid.datamanager.utils.LabelManager
 import es.gob.radarcovid.features.onboarding.pages.legal.protocols.LegalInfoRouter
 import javax.inject.Inject
 
 
-class LegalInfoRouterImpl @Inject constructor(private val fragment: Fragment) : LegalInfoRouter {
+class LegalInfoRouterImpl @Inject constructor(
+    private val fragment: Fragment,
+    private val labelManager: LabelManager
+) : LegalInfoRouter {
 
     override fun navigateToConditions() {
         fragment.startActivity(
             Intent(
                 Intent.ACTION_VIEW,
-                Uri.parse(Constants.URL_USAGE_CONDITIONS)
+                getUsageConditionsUri()
             )
         )
     }
@@ -23,9 +27,27 @@ class LegalInfoRouterImpl @Inject constructor(private val fragment: Fragment) : 
         fragment.startActivity(
             Intent(
                 Intent.ACTION_VIEW,
-                Uri.parse(Constants.URL_PRIVACY_POLICY)
+                getPrivacyPolicyUri()
             )
         )
+    }
+
+    private fun getUsageConditionsUri(): Uri {
+        val url =
+            labelManager.getText("USE_CONDITIONS_URL", Constants.URL_USAGE_CONDITIONS).toString()
+        return if (url.contains("http://") || url.contains("https://"))
+            Uri.parse(url)
+        else
+            Uri.parse("http://$url")
+    }
+
+    private fun getPrivacyPolicyUri(): Uri {
+        val url =
+            labelManager.getText("PRIVACY_POLICY_URL", Constants.URL_PRIVACY_POLICY).toString()
+        return if (url.contains("http://") || url.contains("https://"))
+            Uri.parse(url)
+        else
+            Uri.parse("http://$url")
     }
 
 }
