386399a1eac2fcf3b5ac7a9d8253b9ed9d2e54bd
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-08-06 13:43:13 +0200

    *[0027923]

--- app/src/main/java/es/gob/radarcovid/features/home/view/HomeFragment.kt
+++ app/src/main/java/es/gob/radarcovid/features/home/view/HomeFragment.kt
@@ -207,7 +207,12 @@ class HomeFragment : BaseFragment(), HomeView {
 
     override fun showUnableToReportCovidDialog() {
         CMDialog.Builder(context!!)
-            .setMessage(R.string.covid_report_warning)
+            .setMessage(
+                labelManager.getText(
+                    "ALERT_RADAR_REQUIRED_TO_REPORT",
+                    R.string.covid_report_warning
+                ).toString()
+            )
             .setPositiveButton(
                 labelManager.getText(
                     "ALERT_ACCEPT_BUTTON",
