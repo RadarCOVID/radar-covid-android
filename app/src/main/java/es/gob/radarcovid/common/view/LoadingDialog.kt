a62527fc9eaaca8942cd8602435433480e94fcf5
Alberto Vecina Sánchez <avecina@eservicios.indra.es>
2020-09-14 09:12:29 +0200

    *[Loading] Implemented fix for the loading dialog not dismissing

--- app/src/main/java/es/gob/radarcovid/common/view/LoadingDialog.kt
+++ app/src/main/java/es/gob/radarcovid/common/view/LoadingDialog.kt
@@ -44,18 +44,12 @@ class LoadingDialog(context: Context) :
             WindowManager.LayoutParams.MATCH_PARENT
         )
 
-        initViews()
-
         viewBackground.startAnimation(AlphaAnimation(0f, viewBackground.alpha).apply {
             duration = fadeInDuration
         })
     }
 
-    private fun initViews() {
-        buttonClose.setOnClickListener { hide() }
-    }
-
-    override fun hide() {
+    fun dismissWithAnimation() {
         if (!isHiding) {
             isHiding = true
             viewBackground.startAnimation(AlphaAnimation(viewBackground.alpha, 0f).apply {
@@ -66,7 +60,7 @@ class LoadingDialog(context: Context) :
                     }
 
                     override fun onAnimationEnd(animation: Animation?) {
-                        super@LoadingDialog.hide()
+                        super@LoadingDialog.dismiss()
                         isHiding = false
                     }
 
@@ -79,6 +73,7 @@ class LoadingDialog(context: Context) :
     }
 
     fun showError(title: String?, message: String?, button: String, onClick: (() -> Unit) = {}) {
+        buttonClose.setOnClickListener { dismissWithAnimation() }
         title?.let {
             textViewDialogTitle.visibility = View.VISIBLE
             textViewDialogTitle.text = title
@@ -89,7 +84,7 @@ class LoadingDialog(context: Context) :
         }
         buttonOk.text = button
         buttonOk.setOnClickListener {
-            hide()
+            dismissWithAnimation()
             onClick()
         }
 
